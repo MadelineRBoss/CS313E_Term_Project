e31f431247287f4558950a951a537555424c099c
Madeline Boss <147550251+MadelineRBoss@users.noreply.github.com>
2024-03-24 13:55:26 -0500

    Updated File with Outline

--- Term_project.py
+++ Term_project.py
@@ -2,5 +2,41 @@
 Term Project
 
 Team Member 1: Madeline Boss (mrb5727)
-Team Member 2: Tariq Ali (taa2536)
+Team Member 2: Tariq
 """
+
+def bootstrap(base_sample, num):
+    """
+    inputs:
+    base_sample: sample to create synethic samples
+    num: number of samples to be creted
+
+    output:
+    final_list: list of final bootstrapped samples
+    """
+    pass
+
+class BST():
+    """
+    Binary Search Tree
+    """
+    def __init__():
+        pass
+
+    def create_tree():
+        pass
+
+    def medain():
+        pass
+
+    def range():
+        pass
+
+    def mean():
+        pass
+
+def main():
+    sample_input = input("What is your orginal sample? ")
+    num_samples_input = input("How many bootstrap samples do you want to make (limit is 50)? ")
+
+    
