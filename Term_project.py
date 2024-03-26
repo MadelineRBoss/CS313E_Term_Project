c7d95951749a7b26d7416c1989acf1584b22b1ba
Madeline Boss <147550251+MadelineRBoss@users.noreply.github.com>
2024-03-26 13:01:14 -0500

    Better standard error logic

--- Term_project.py
+++ Term_project.py
@@ -9,8 +9,8 @@ import math
 
 #dictonary of z values:
 z_star_dict = {0.001: 3.090, 0.005: 2.576, 0.025: 1.960, 0.05: 1.645, 0.10: 1.282,
-                0.15: 1.036, 0.20: 0.842, 0.25: 0.674, 0.30: 0.524, 0.35: 0.385, 
-                0.40: 0.253, 0.45: 0.125, 0.50: 0.000,}
+                0.15: 1.036, 0.20: 0.842, 0.25: 0.674, 0.30: 0.524, 0.35: 0.385,
+                0.40: 0.253, 0.45: 0.125, 0.50: 0.000}
 
 # Code can handle resampling of up to 10,000 with no real issue. Limitations for resampling will be based on binary tree run time
 def bootstrap(base_sample, num):
@@ -91,30 +91,28 @@ class BootstrapBST():
         """
         #To be implemented
         return 1
-
-    def sd_local(self):
+    
+    def sd(self):
         """
-        finds standard deivation of a single sample
+        gets standard deivation
         """
         #get local mean
-        local_mean = 0
-        for num in self.base:
-            local_mean += num
-        local_mean /= len(self.base)
+        local_mean = self.mean()
 
         #get sum of variance squared
         sum_of_sqaures = 0
-        for num in self.base:
+        for num in self.sorted_tree():
             #finds variance then square
-            sum_of_sqaures += (num - local_mean)**2
+            sum_of_sqaures += ((num - local_mean) ** 2) / (len(self.base()) - 1)
         #returns local sd
-        return math.sqrt(sum_of_sqaures/len(self.base)
+        return sum_of_sqaures
 
-    def sd_sample_distrubution(self):
+    def se(self):
         """
-        finds standard deivation of a whole BST
+        gets standard error
         """
-        return self.sd_local()/math.sqrt(self.length())
+        return self.sd()/math.sqrt(self.length())
+
 
     def ci(self, alpha = 0.05):
         """
@@ -126,7 +124,7 @@ class BootstrapBST():
         output max: the maximum value in the CI
         """
         bst_mean = self.mean()
-        bst_sd = self.sd_sample_distrubution()
+        bst_se = self.se()
 
         #checks if alpha is in the z star table then grabs it
         #potentially add binary search algorthim for next best value (lower value) instead of raising exceptions? <-- Get's Tariq's opinion
@@ -134,8 +132,8 @@ class BootstrapBST():
             raise NotImplementedError("z*star value not implemented")
         z_star = z_star_dict[alpha]
 
-        ci_min = bst_mean - z_star*bst_sd
-        ci_max = bst_mean + z_star*bst_sd
+        ci_min = bst_mean - z_star*bst_se
+        ci_max = bst_mean + z_star*bst_se
 
         return ci_min, ci_max
 
@@ -147,6 +145,17 @@ class BootstrapBST():
         if ci_min < value < ci_max:
             return True
         return False
+    
+    def test_mean(self, test_mean):
+        """
+        finds a chance of a mean being reasonable in a sampling distrubution
+        """
+        chance = 0
+        values = self.sorted_tree()
+        for val in values:
+            if val >= test_mean:
+                chance += 1
+        return round(chance/self.length(), 4)
 
 
 def main():
