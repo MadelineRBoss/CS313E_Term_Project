a12d3cf9b04b884e329b7559b3cb1bbb63e0aed0
Madeline Boss <147550251+MadelineRBoss@users.noreply.github.com>
2024-03-25 19:38:30 -0500

    Added psuedo code for stats anaylsis and notes
    I got really bored ¯\_(ツ)_/¯
    Code to be updated after BST implementation

--- Term_project.py
+++ Term_project.py
@@ -5,6 +5,12 @@ Team Member 1: Madeline Boss (mrb5727)
 Team Member 2: Tariq Ali (taa2536)
 """
 import random
+import math
+
+#dictonary of z values:
+z_star_dict = {0.001: 3.090, 0.005: 2.576, 0.025: 1.960, 0.05: 1.645, 0.10: 1.282,
+                0.15: 1.036, 0.20: 0.842, 0.25: 0.674, 0.30: 0.524, 0.35: 0.385, 
+                0.40: 0.253, 0.45: 0.125, 0.50: 0.000,}
 
 # Code can handle resampling of up to 10,000 with no real issue. Limitations for resampling will be based on binary tree run time
 def bootstrap(base_sample, num):
@@ -30,28 +36,119 @@ def bootstrap(base_sample, num):
     return final_list
 
 class Node():
-    def __init(self):
+    """
+    Nodes for BST
+    """
+    def __init__ (self):
         pass
 
-class BST():
-    def __init__(self):
-        pass
+class BootstrapBST():
+    """
+    BST tree that represents Bootstrap samples
+    """
+    def __init__(self, base):
+        self.base = base  #when intalized, keeps memory of base sample
+        #rest to be implemented
 
     def create_tree(self):
-        pass
+        """
+        create BST
+        """
+        #to be implemented
+        return
 
     def medain(self):
-        pass
+        """
+        returns medain of BST
+        """
+        #to be implemented
+        return
 
     def range(self):
-        pass
+        """
+        return BST range (i.e min and max of BST)
+        """
+        #to be implemented
+        return
 
     def mean(self):
-        pass
+        """
+        return mean of BST
+        """
+        #to be implemented
+        return 0
+
+    def sorted_tree(self):
+        """
+        return the BST sorted
+        """
+        #to be implemented
+        return []
+    
+    def length(self):
+        """
+        return length of BST
+        """
+        #To be implemented
+        return 1
+
+    def sd_local(self):
+        """
+        finds standard deivation of a single sample
+        """
+        #get local mean
+        local_mean = 0
+        for num in self.base:
+            local_mean += num
+        local_mean /= len(self.base)
+
+        #get sum of variance squared
+        sum_of_sqaures = 0
+        for num in self.base:
+            #finds variance then square
+            sum_of_sqaures += (num - local_mean)**2
+        #returns local sd
+        return math.sqrt(sum_of_sqaures/self.length())
+
+    def sd_sample_distrubution(self):
+        """
+        finds standard deivation of a whole BST
+        """
+        return self.sd_local()/math.sqrt(self.length())
+
+    def ci(self, alpha = 0.05):
+        """
+        Finds Confidence Interval (CI)
+
+        input alpha: a value between 0 and .5
+
+        output min: the minimum value in the CI
+        output max: the maximum value in the CI
+        """
+        bst_mean = self.mean()
+        bst_sd = self.sd_sample_distrubution()
+
+        #checks if alpha is in the z star table then grabs it
+        #potentially add binary search algorthim for next best value (lower value) instead of raising exceptions? <-- Get's Tariq's opinion
+        if alpha not in z_star_dict.keys():
+            raise NotImplementedError("z*star value not implemented")
+        z_star = z_star_dict[alpha]
+
+        ci_min = bst_mean - z_star*bst_sd
+        ci_max = bst_mean + z_star*bst_sd
+
+        return ci_min, ci_max
+
+    def stat_signficant(self, value, alpha):
+        """
+        checks if sample value is in CI (or rather, if it's signficant)
+        """
+        ci_min, ci_max = self.ci(alpha)
+        if ci_min < value < ci_max:
+            return True
+        return False
+
 
 def main():
     sample_input = input("What is your orginal sample? ")
     num_samples_input = input("How many bootstrap samples do you want to make (limit is 50)? ")
-
-
-    
