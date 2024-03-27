08599e0c154005be32ab97fc25fcf7c6b9f12285
Madeline Boss <147550251+MadelineRBoss@users.noreply.github.com>
2024-03-27 09:45:09 -0500

    More accurate standard error logic

--- Term_project.py
+++ Term_project.py
@@ -8,12 +8,11 @@ import random
 import math
 
 #dictonary of z values:
-z_star_dict = {0.001: 3.090, 0.005: 2.576, 0.025: 1.960, 0.05: 1.645, 0.10: 1.282,
-                0.15: 1.036, 0.20: 0.842, 0.25: 0.674, 0.30: 0.524, 0.35: 0.385,
-                0.40: 0.253, 0.45: 0.125, 0.50: 0.000}
+z_star_dict = {0.80: 1.2816, 0.85: 1.4408, 0.90: 1.6449, 0.95: 1.9600,
+               0.975: 2.2414, 0.99: 2.5760, 0.995: 2.8070, 0.999: 3.2910}
 
-# Code can handle resampling of up to 10,000 with no real issue. Limitations for resampling will be based on binary tree run time
-def bootstrap(base_sample, num):
+#Code can handle resampling of up to 10,000 with no real issue. Limitations for resampling will be based on binary tree run time
+def bootstrap(base_sample):
     """
     inputs:
     base_sample: sample to create synethic samples
@@ -22,25 +21,37 @@ def bootstrap(base_sample, num):
     output:
     final_list: list of final bootstrapped samples
     """
-    final_list  = []
     sample_size = len(base_sample)
+    boot_sample = []
 
-    for __ in range(num):
-        temp_total = 0
-        for __ in range(sample_size):
-            random_pos = random.randint(0, sample_size - 1)
-            temp_total += base_sample[random_pos]
-        bootstrap_sample = temp_total/sample_size
-        final_list.append(bootstrap_sample)
+    temp_total = 0
+    #create bootstrap sample
+    for __ in range(sample_size):
+        random_pos = random.randint(0, sample_size - 1)
+        boot_sample.append(base_sample[random_pos])
+        temp_total += base_sample[random_pos]
+    
+    #get mean
+    bootstrap_mean = temp_total/sample_size
 
-    return final_list
+    #get sd
+    square_of_sums = 0
+    for num in boot_sample:
+        square_of_sums += ((num - bootstrap_mean) ** 2)
+    square_of_sums /= sample_size
+    bootstrap_sd = math.sqrt(square_of_sums)
+
+    return round(bootstrap_mean,2), round(bootstrap_sd,2)
 
 class Node():
     """
     Nodes for BST
     """
-    def __init__ (self):
-        pass
+    def __init__ (self, data_mean, data_sd, lchild = None, rchild = None):
+        self.mean = data_mean   # tree is based on mean value
+        self.sd = data_sd   # another data value that is stored for stats anaylsis 
+        self.lchild = lchild
+        self.rchild = rchild
 
 class BootstrapBST():
     """
@@ -50,9 +61,9 @@ class BootstrapBST():
         self.base = base  #when intalized, keeps memory of base sample
         #rest to be implemented
 
-    def create_tree(self):
+    def add_tree_value(self):
         """
-        create BST
+        Add to tree
         """
         #to be implemented
         return
@@ -84,41 +95,31 @@ class BootstrapBST():
         """
         #to be implemented
         return []
-    
+
     def length(self):
         """
         return length of BST
         """
         #To be implemented
         return 1
-    
-    def sd(self):
-        """
-        gets standard deivation
-        """
-        #get local mean
-        local_mean = self.mean()
-
-        #get sum of variance squared
-        sum_of_sqaures = 0
-        for num in self.sorted_tree():
-            #finds variance then square
-            sum_of_sqaures += ((num - local_mean) ** 2) / (len(self.base()) - 1)
-        #returns local sd
-        return sum_of_sqaures
 
     def se(self):
         """
         gets standard error
         """
-        return self.sd()/math.sqrt(self.length())
+        #Need to find a way to loop through sd values (another class method maybe?)
+        sum_of_sd = 0
+        for sd in sd_values:
+            sum_of_sd += sd
+        se = sum_of_sd/self.length()
 
+        return se
 
-    def ci(self, alpha = 0.05):
+    def ci(self, ci_percentage = 0.95):
         """
         Finds Confidence Interval (CI)
 
-        input alpha: a value between 0 and .5
+        input ci_percentage: a value between 0 and .5
 
         output min: the minimum value in the CI
         output max: the maximum value in the CI
@@ -126,26 +127,26 @@ class BootstrapBST():
         bst_mean = self.mean()
         bst_se = self.se()
 
-        #checks if alpha is in the z star table then grabs it
+        #checks if ci_percentage is in the z star table then grabs it
         #potentially add binary search algorthim for next best value (lower value) instead of raising exceptions? <-- Get's Tariq's opinion
-        if alpha not in z_star_dict.keys():
+        if ci_percentage not in z_star_dict.keys():
             raise NotImplementedError("z*star value not implemented")
-        z_star = z_star_dict[alpha]
+        z_star = z_star_dict[ci_percentage]
 
         ci_min = bst_mean - z_star*bst_se
         ci_max = bst_mean + z_star*bst_se
 
         return ci_min, ci_max
 
-    def stat_signficant(self, value, alpha):
+    def stat_signficant(self, value, ci_percentage):
         """
         checks if sample value is in CI (or rather, if it's signficant)
         """
-        ci_min, ci_max = self.ci(alpha)
+        ci_min, ci_max = self.ci(ci_percentage)
         if ci_min < value < ci_max:
             return True
         return False
-    
+
     def test_mean(self, test_mean):
         """
         finds a chance of a mean being reasonable in a sampling distrubution
@@ -159,5 +160,8 @@ class BootstrapBST():
 
 
 def main():
+    """
+    mainline logic
+    """
     sample_input = input("What is your orginal sample? ")
     num_samples_input = input("How many bootstrap samples do you want to make (limit is 50)? ")
